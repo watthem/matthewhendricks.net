c7daa98bc9ef75a60010e64423d2ca18c26077cf
Matthew Hendricks <mscohendricks@gmail.com>
2021-08-26 21:36:42 -0700

    iframe fix

--- src/components/NewsLetterForm.vue
+++ src/components/NewsLetterForm.vue
@@ -1,5 +1,13 @@
 <template>
   <section>
+    <h2 class="text-2xl font-semibold text-gray-800 dark:text-white">
+      Subscribe to my
+      <a
+        href="https://matthewhendicks.substack.com"
+        class="text-blue-500 underline"
+        >Newsletter</a
+      >
+    </h2>
     <!-- newsletter sign-up -->
     <div class="container px-6 py-16 mx-auto">
       <div class="items-center">
@@ -12,23 +20,10 @@
         </div>
       </div>
       <div class="bg-gray-100">
-        <h2 class="text-2xl font-semibold text-gray-800 dark:text-white">
-          Subscribe to my
-          <a
-            href="https://matthewhendicks.substack.com"
-            class="text-blue-500 underline"
-            >Newsletter</a
-          >
-        </h2>
-        <p class="mt-4 text-gray-600 dark:text-gray-400">
-          Be the first to knows when my
-          <g-link to="/blog" class="font-medium text-blue-500 underline"
-            >blog content</g-link
-          >
-          is published
-        </p>
-        <div class="w-1/4">
+        <div class="w-full">
           <iframe
+            class="m-auto w-full"
+            height="500"
             src="https://matthewhendricks.substack.com/embed"
             frameborder="0"
             scrolling="no"
